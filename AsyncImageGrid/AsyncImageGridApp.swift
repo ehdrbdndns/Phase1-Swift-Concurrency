67006ea21f6e854eb1a9eddf503b87e9b091ee3e
ehdrbdndns <ehdrbdndns@naver.com>
2025-07-24 16:59:05 +0900

    Initial Commit

---
+++ AsyncImageGrid/AsyncImageGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageGridApp.swift
+//  AsyncImageGrid
+//
+//  Created by Donggyun Yang on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
